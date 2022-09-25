a689e0abe2f0ff461c7769b837801d37ee396799
wangze1452 <114323385+wangze1452@users.noreply.github.com>
2022-09-25 11:47:51 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,12 +7,12 @@ const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: '',
+  APP_ID: 'wx6eb85f228117bfcd',
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: '',
+  APP_SECRET: 'c3544e4f0397813fa2d5da4b1b7cddc0',
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -60,20 +60,20 @@ const USER_CONFIG = {
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
-  USERS: [
+  USERS: [ompt75sQfa265tJvy8_lWMkxqFsY，ompt75miRkGRC561wAgfn0JXuRIQ
     {
       // 想要发送的人的名字
-      name: '老婆0',
+      name: '宝',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
       // 你想对他发送的模板消息的模板ID
       useTemplateId: '',
       // 所在省份
-      province: '广东',
+      province: '潍坊市',
       // 所在城市
-      city: '肇庆',
+      city: '潍坊',
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate: '01-14',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
@@ -98,11 +98,11 @@ const USER_CONFIG = {
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-09-23' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-        // 退伍日
-        { keyword: 'ex_day', date: '2022-09-10' }
+        { keyword: 'marry_day', date: '' },
+      
+   
       ],
       // 课程表相关配置
       // 如果courseSchedule不存在或者为空（null）则认为没有课程
@@ -171,7 +171,7 @@ const USER_CONFIG = {
       },
     },
     {
-      name: '老婆1',
+      name: '宝',
       id: '',
       useTemplateId: '',
       province: '',
@@ -184,7 +184,7 @@ const USER_CONFIG = {
       courseSchedule: null
     },
     {
-      name: '老婆2',
+      name: '宝宝',
       id: '',
       useTemplateId: '',
       province: '',
@@ -197,7 +197,7 @@ const USER_CONFIG = {
       courseSchedule: null
     },
     {
-      name: '老婆3',
+      name: '大宝',
       id: '',
       useTemplateId: '',
       province: '',
@@ -239,9 +239,9 @@ const USER_CONFIG = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: '广东',
+  PROVINCE: '潍坊',
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: '惠州',
+  CITY: '潍坊',
 
   /** 重要节日相关 */
 
